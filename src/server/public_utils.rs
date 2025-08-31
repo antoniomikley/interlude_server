8606235173dda2fad6ec203d4e9ece0633b1f6c2
Lennsorid Strubbqal <stubbelennard135@gmail.com>
2025-08-31 11:37:21 +0200

    Added official logo and icon

--- src/server/public_utils.rs
+++ src/server/public_utils.rs
@@ -7,6 +7,8 @@ use serde::{Deserialize, Serialize};
 pub struct Provider {
     name: String,
     url: String,
+    #[serde(rename = "logoUrl")]
+    logo_url: String,
     #[serde(rename = "iconUrl")]
     icon_url: String,
 }
@@ -16,12 +18,14 @@ pub fn get_providers() -> Vec<Provider> {
         Provider {
             name: "Spotify".to_string(),
             url: "https://spotify.com".to_string(),
-            icon_url: "https://interlude.api.leshift.de/public/spotify.png".to_string(),
+            logo_url: "https://interlude.api.leshift.de/public/spotify_logo.png".to_string(),
+            icon_url: "https://interlude.api.leshift.de/public/spotify_icon.png".to_string(),
         },
         Provider {
             name: "Tidal".to_string(),
             url: "https://tidal.com".to_string(),
-            icon_url: "https://interlude.api.leshift.de/public/tidal.png".to_string(),
+            logo_url: "https://interlude.api.leshift.de/public/tidal_logo.png".to_string(),
+            icon_url: "https://interlude.api.leshift.de/public/tidal_icon.png".to_string(),
         },
     ]
 }
